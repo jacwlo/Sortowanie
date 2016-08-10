02a1c645e8f8c02bab7a60e62261d3ce818b897f
fly <fly@mikrolabik>
2016-08-10 13:24:08 +0200

    fix display case

--- Sortowanie/src/sortowanie/Sort.java
+++ Sortowanie/src/sortowanie/Sort.java
@@ -5,8 +5,11 @@ public class Sort {
 	public static void main(String[] args) {
 		System.out.println("Witamy");
 		int[] tablica = {2,3,1,5,4,6,7,8,9,10};
-		System.out.print("Wprowadzone liczby: ");
-		System.out.println(tablica[4]);
+		System.out.println("Wprowadzone liczby: ");
+		int i;
+		for (i=0; i<9; i++){
+		System.out.println(tablica[i]);
+		};
 	}
 
 }
