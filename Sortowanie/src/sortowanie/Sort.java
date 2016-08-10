6533a959540457f4ee7cd77c64f3b76217dc0a00
fly <fly@mikrolabik>
2016-08-10 13:16:42 +0200

    init ad display data

--- Sortowanie/src/sortowanie/Sort.java
+++ Sortowanie/src/sortowanie/Sort.java
@@ -5,7 +5,8 @@ public class Sort {
 	public static void main(String[] args) {
 		System.out.println("Witamy");
 		int[] tablica = {2,3,1,5,4,6,7,8,9,10};
-	
+		System.out.print("Wprowadzone liczby: ");
+		System.out.println(tablica[4]);
 	}
 
 }
