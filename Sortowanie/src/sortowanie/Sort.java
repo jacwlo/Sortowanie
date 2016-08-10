40927fb925a68ba212cd22f257cafa42def880b5
fly <fly@mikrolabik>
2016-08-10 14:06:24 +0200

    wprowadzanie

--- Sortowanie/src/sortowanie/Sort.java
+++ Sortowanie/src/sortowanie/Sort.java
@@ -1,14 +1,23 @@
 package sortowanie;
 
+import java.util.Scanner;
+
 public class Sort {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		/**Wprowadzenie d³ugoœci tablicy**/
 		System.out.println("Witamy");
-		int[] tablica = {2,3,1,5,4,6,7,8,9,10};
-		System.out.println("Wprowadzone liczby: ");
-		int i;
-		for (i=0; i<10; i++){
-		System.out.println(tablica[i]);
+		System.out.print("Podaj wielkoœæ tablicy: ");
+		int a = in.nextInt();
+		int[] tab = new int[a];
+		System.out.format("Wielkoœæ tablicy: %d", tab.length);
+		
+		for (int i=0; i<tab.length; i++){
+		System.out.format("\nPodaj %d element tablicy: ", i+1);
+		int b = in.nextInt();
+		tab[i]=b;
+		System.out.format("Wprowadzony %d element to: %d", i, tab[i]);
 		};
 	}
 
